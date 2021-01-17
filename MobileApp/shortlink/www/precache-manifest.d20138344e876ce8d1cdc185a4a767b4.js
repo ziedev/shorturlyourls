04fe83496fc1c24fa65fecf6af0df7fc66994bbc
Zied Maaloul <me@ziedmaaloul.site>
2021-01-17 20:25:12 +0100

    Big Update

---
+++ MobileApp/shortlink/www/precache-manifest.d20138344e876ce8d1cdc185a4a767b4.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303eff14a22ea5f5d825",
+    "url": "css/app.bfbafe73.css"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "a794a15f6e45b941d8a521e7965221a8",
+    "url": "img/brand/favicon.png"
+  },
+  {
+    "revision": "59d7c2b119ab46fc7b5f853f04324c89",
+    "url": "img/brand/green.png"
+  },
+  {
+    "revision": "e0ea3f1916671872498288dc2bd40f06",
+    "url": "img/brand/white.png"
+  },
+  {
+    "revision": "aab189ad3f0df59adac0c4a87107bbb5",
+    "url": "img/logos/logo.svg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "1f533d2fdc40493ee834dd8e4288220c",
+    "url": "img/theme/angular.jpg"
+  },
+  {
+    "revision": "d136a071cf09ba5ab8dc2ada1ab04015",
+    "url": "img/theme/bootstrap.jpg"
+  },
+  {
+    "revision": "cee60cfd101f9b3d2aae0e819ec8b267",
+    "url": "img/theme/profile-cover.jpg"
+  },
+  {
+    "revision": "50dd5fdc839dfc16739b15370650485c",
+    "url": "img/theme/react.jpg"
+  },
+  {
+    "revision": "3b8c576082e5ca27bae21a839f1290f6",
+    "url": "img/theme/sketch.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "c532f6f0176968caa6265864ee1abb35",
+    "url": "img/theme/vue.jpg"
+  },
+  {
+    "revision": "c4d375aac2a848645887fce5c7cb7e46",
+    "url": "index.html"
+  },
+  {
+    "revision": "303eff14a22ea5f5d825",
+    "url": "js/app.7063f4b1.js"
+  },
+  {
+    "revision": "b03dae75e0f05b044cfa",
+    "url": "js/chunk-vendors.4eb60e80.js"
+  },
+  {
+    "revision": "5b23ab92670b6f7fff0a",
+    "url": "js/demo.85e317f2.js"
+  },
+  {
+    "revision": "2c6190eb814d2dd4d454ae170c43c7c2",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
